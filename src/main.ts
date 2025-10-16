17de720a974aa1488502e5f6f6314d0481bffbc4
Nikolaenko <aleshka.dev@gmail.com>
2025-10-16 10:54:12 +0300

    base config setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  app.enableCors({
+    methods: ['POST', 'GET', 'PATCH', 'DELETE'],
+    origin: configService.getOrThrow<string>('ORIGIN_URL'),
+    credentials: true,
+  });
+
+  await app.listen(configService.getOrThrow<number>('PORT'));
 }
 bootstrap().catch((e) => console.log(e));
